cfb431769d3f6d25baa2cba626571d942c03f070
kalpana0824 <159270019+kalpana0824@users.noreply.github.com>
2024-12-28 17:07:46 +0530

    Update check.py

--- check.py
+++ check.py
@@ -5,7 +5,7 @@ import seaborn as sns
 from streamlit_option_menu import option_menu
 
 # Loading the saved models
-diabetes_model = pickle.load(open('https://github.com/kalpana0824/predicition/blob/main/diabetes_model.sav, 'rb'))
+diabetes_model = pickle.load(open('https://github.com/kalpana0824/predicition/blob/main/diabetes_model.sav', 'rb'))
 heart_disease_model = pickle.load(open("/workspaces/predicition/heart_model.sav", 'rb'))
 parkinsons_model = pickle.load(open("/workspaces/predicition/parkinsons_model.sav", 'rb'))
 
